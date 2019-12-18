8597e046f4f8e8d80d79f375935b42c9f5209b98
Hui Yang <chy1988@antwebstudio.com>
2019-12-18 12:43:05 +0800

    First commit

---
+++ src/Installer.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace ant\moduleManager;
+
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+/*use Composer\Script\CommandEvent;
+use Composer\Script\Event;
+use Composer\Util\Filesystem;*/
+
+class Installer extends \Composer\Installer\LibraryInstaller
+{
+    //const EXTRA_BOOTSTRAP = 'bootstrap';
+    const FILENAME = 'antweb/modules.php';
+	const COMPOSER_PACKAGE_TYPE = 'ant-module';
+
+
+    /**
+     * @inheritdoc
+     */
+    public function supports($packageType)
+    {
+        return $packageType === static::COMPOSER_PACKAGE_TYPE;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        // install the package the normal composer way
+        parent::install($repo, $package);
+        // add the package to yiisoft/extensions.php
+        $this->addPackage($package);
+        // ensure the yii2-dev package also provides Yii.php in the same place as yii2 does
+        /*if ($package->getName() == 'yiisoft/yii2-dev') {
+            $this->linkBaseYiiFiles();
+        }*/
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+    {
+        parent::update($repo, $initial, $target);
+        $this->removePackage($initial);
+        $this->addPackage($target);
+        // ensure the yii2-dev package also provides Yii.php in the same place as yii2 does
+        /*if ($initial->getName() == 'yiisoft/yii2-dev') {
+            $this->linkBaseYiiFiles();
+        }*/
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        // uninstall the package the normal composer way
+        parent::uninstall($repo, $package);
+        // remove the package from yiisoft/extensions.php
+        $this->removePackage($package);
+        // remove links for Yii.php
+        /*if ($package->getName() == 'yiisoft/yii2-dev') {
+            $this->removeBaseYiiFiles();
+        }*/
+    }
+
+    protected function addPackage(PackageInterface $package)
+    {
+        $extension = [
+            'name' => $package->getName(),
+            'version' => $package->getVersion(),
+        ];
+
+        /*$alias = $this->generateDefaultAlias($package);
+        if (!empty($alias)) {
+            $extension['alias'] = $alias;
+        }
+        $extra = $package->getExtra();
+        if (isset($extra[self::EXTRA_BOOTSTRAP])) {
+            $extension['bootstrap'] = $extra[self::EXTRA_BOOTSTRAP];
+        }*/
+
+        $extensions = $this->loadModules();
+        $extensions[$package->getName()] = $extension;
+        $this->saveModules($extensions);
+    }
+
+    /*protected function generateDefaultAlias(PackageInterface $package)
+    {
+        $fs = new Filesystem;
+        $vendorDir = $fs->normalizePath($this->vendorDir);
+        $autoload = $package->getAutoload();
+
+        $aliases = [];
+
+        if (!empty($autoload['psr-0'])) {
+            foreach ($autoload['psr-0'] as $name => $path) {
+                $name = str_replace('\\', '/', trim($name, '\\'));
+                if (!$fs->isAbsolutePath($path)) {
+                    $path = $this->vendorDir . '/' . $package->getPrettyName() . '/' . $path;
+                }
+                $path = $fs->normalizePath($path);
+                if (strpos($path . '/', $vendorDir . '/') === 0) {
+                    $aliases["@$name"] = '<vendor-dir>' . substr($path, strlen($vendorDir)) . '/' . $name;
+                } else {
+                    $aliases["@$name"] = $path . '/' . $name;
+                }
+            }
+        }
+
+        if (!empty($autoload['psr-4'])) {
+            foreach ($autoload['psr-4'] as $name => $path) {
+                if (is_array($path)) {
+                    // ignore psr-4 autoload specifications with multiple search paths
+                    // we can not convert them into aliases as they are ambiguous
+                    continue;
+                }
+                $name = str_replace('\\', '/', trim($name, '\\'));
+                if (!$fs->isAbsolutePath($path)) {
+                    $path = $this->vendorDir . '/' . $package->getPrettyName() . '/' . $path;
+                }
+                $path = $fs->normalizePath($path);
+                if (strpos($path . '/', $vendorDir . '/') === 0) {
+                    $aliases["@$name"] = '<vendor-dir>' . substr($path, strlen($vendorDir));
+                } else {
+                    $aliases["@$name"] = $path;
+                }
+            }
+        }
+
+        return $aliases;
+    }*/
+
+    protected function removePackage(PackageInterface $package)
+    {
+        $packages = $this->loadModules();
+        unset($packages[$package->getName()]);
+        $this->saveModules($packages);
+    }
+
+    protected function loadModules()
+    {
+        $file = $this->vendorDir . '/' . static::FILENAME;
+        if (!is_file($file)) {
+            return [];
+        }
+        // invalidate opcache of modules.php if exists
+        if (function_exists('opcache_invalidate')) {
+            @opcache_invalidate($file, true);
+        }
+        $extensions = require($file);
+
+        $vendorDir = str_replace('\\', '/', $this->vendorDir);
+        $n = strlen($vendorDir);
+
+        foreach ($extensions as &$extension) {
+            if (isset($extension['alias'])) {
+                foreach ($extension['alias'] as $alias => $path) {
+                    $path = str_replace('\\', '/', $path);
+                    if (strpos($path . '/', $vendorDir . '/') === 0) {
+                        $extension['alias'][$alias] = '<vendor-dir>' . substr($path, $n);
+                    }
+                }
+            }
+        }
+
+        return $extensions;
+    }
+
+    protected function saveModules(array $extensions)
+    {
+        $file = $this->vendorDir . '/' . static::FILENAME;
+        if (!file_exists(dirname($file))) {
+            mkdir(dirname($file), 0777, true);
+        }
+        $array = str_replace("'<vendor-dir>", '$vendorDir . \'', var_export($extensions, true));
+        file_put_contents($file, "<?php\n\n\$vendorDir = dirname(__DIR__);\n\nreturn $array;\n");
+        // invalidate opcache of extensions.php if exists
+        if (function_exists('opcache_invalidate')) {
+            @opcache_invalidate($file, true);
+        }
+    }
+
+    /**
+     * Special method to run tasks defined in `[extra][yii\composer\Installer::postCreateProject]` key in `composer.json`
+     *
+     * @param Event $event
+     */
+    public static function postCreateProject($event)
+    {
+        static::runCommands($event, __METHOD__);
+    }
+
+    /**
+     * Special method to run tasks defined in `[extra][yii\composer\Installer::postInstall]` key in `composer.json`
+     *
+     * @param Event $event
+     * @since 2.0.5
+     */
+    public static function postInstall($event)
+    {
+        static::runCommands($event, __METHOD__);
+    }
+
+    /**
+     * Special method to run tasks defined in `[extra][$extraKey]` key in `composer.json`
+     *
+     * @param Event $event
+     * @param string $extraKey
+     * @since 2.0.5
+     */
+    protected static function runCommands($event, $extraKey)
+    {
+        $params = $event->getComposer()->getPackage()->getExtra();
+        if (isset($params[$extraKey]) && is_array($params[$extraKey])) {
+            foreach ($params[$extraKey] as $method => $args) {
+                call_user_func_array([__CLASS__, $method], (array) $args);
+            }
+        }
+    }
+}
---
+++ src/Plugin.php
@@ -0,0 +1,38 @@
+<?php
+namespace ant\moduleManager;
+
+use Composer\Composer;
+use Composer\Plugin\PluginInterface;
+use Composer\IO\IOInterface;
+
+class Plugin implements PluginInterface
+{
+    /**
+     * @var array noted package updates.
+     */
+    private $_packageUpdates = [];
+    /**
+     * @var string path to the vendor directory.
+     */
+    private $_vendorDir;
+	
+	protected $_vendorName = 'antweb';
+
+	protected $_moduleFilename = 'modules.php';
+
+    /**
+     * @inheritdoc
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+        $this->_vendorDir = rtrim($composer->getConfig()->get('vendor-dir'), '/');
+        $file = $this->_vendorDir . '/'.$this->_vendorName.'/'.$this->_moduleFilename;
+		
+        if (!is_file($file)) {
+            @mkdir(dirname($file), 0777, true);
+            file_put_contents($file, "<?php\n\nreturn [];\n");
+        }
+    }
+}
